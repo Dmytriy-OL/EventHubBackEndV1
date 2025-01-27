5571ac5fbaa89fe87a25528a43d4cff8d7419bbb
ROI <ipz222_roi@student.ztu.edu.ua>
2025-01-28 00:26:38 +0200

    Move all changes to drf_v1 branch

---
+++ eventhub/event/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="Url")
+    authorId = models.CharField(max_length=50, default="id1111")
+    data = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='images/%Y/%m/%d', blank=False)
+    description = models.TextField(blank=True, null=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('show_event', kwargs={'event_slug': self.slug})
+
+    class Meta:
+        verbose_name = 'Event'
+        verbose_name_plural = 'All Events'
+        ordering = ['data', 'name']
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="Url")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('show_category', kwargs={'category_slug': self.slug})
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        # ordering = ['-name']
---
+++ eventhub/eventhub/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+from eventhub import settings
+from event.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('event.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = pageNotFound
