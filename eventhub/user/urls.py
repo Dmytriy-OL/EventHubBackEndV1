dbe0b12e6570802bc2b8b7b55b6cf280cada1878
ROI <ipz222_roi@student.ztu.edu.ua>
2025-02-10 18:43:12 +0200

    add url to personal_page

--- eventhub/user/urls.py
+++ eventhub/user/urls.py
@@ -4,6 +4,6 @@ from user.views import *
 
 urlpatterns = [
     path('users/', users, name='users'),
-    path('personal_page/<slug:author_slug>/', personal_page, name='personal_page'),
+        path('personal_page/<slug:author_slug>/', personal_page, name='personal_page'),
 
 ]
