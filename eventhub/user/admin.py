c1b5920976891e5c82fd033a0fce6cb7899e2ef7
ROI <ipz222_roi@student.ztu.edu.ua>
2025-02-12 17:50:33 +0200

    add some api endpoints and fixed some bugs

--- eventhub/user/admin.py
+++ eventhub/user/admin.py
@@ -4,9 +4,9 @@ from user.models import Author
 
 
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'surname', 'slug', 'avatar')
+    list_display = ('id', 'nickname','name', 'surname', 'slug', 'avatar', 'banner')
     list_display_links = ('id', 'name')
-    prepopulated_fields = {'slug': ('name',)}
+    prepopulated_fields = {'slug': ('nickname',)}
 
 
 admin.site.register(Author, AuthorAdmin)
